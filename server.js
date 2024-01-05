983cc7cc8cde37e6182227207888544655435025
Chance Creger <chance.creger@gmail.com>
2024-01-05 16:31:41 -0700

    added server.js boilerplate and began get routes and api get routes.

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const uuid = require("uuid");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static("public"));
+//http:localhost:3000/public/notes.html
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+app.get("/api/notes", (req, res) => {
+  const notesData = JSON.parse(fs.readFileSync("./db.json", "utf8"));
+  res.json(notesData);
+});
